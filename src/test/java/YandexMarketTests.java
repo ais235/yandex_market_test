b19465d5e9a8b36d5550c897cb7e088fd9ff48d6
ais235 <ais235@yandex.ru>
2020-12-15 16:25:21 +0300

    first commit

---
+++ src/test/java/YandexMarketTests.java
@@ -0,0 +1,26 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class YandexMarketTests {
+
+    @Test
+    void selenideSearchTest(){
+        //открыть яндекс
+        open("https://yandex.ru");
+        //Кликнуть на «Маркет»
+        $("a[data-id='market']").click();
+        //переход на соседнюю вкладку
+        switchTo().window(1);
+        //В поле поиска ввести «ноутбуки»
+        $("#header-search").setValue("ноутбуки");
+        //Проверить что поле поиска содержит «ноутбуки»
+        $("#header-search").shouldHave(Condition.value("ноутбуки"));
+        //Нажать «Найти»
+        $("button[type='submit']").click();
+        //Проверяем что нашлись ноутбуки
+        $("div[data-apiary-widget-name='@MarketNode/SearchHeadline']").shouldHave(Condition.text("Ноутбуки"));
+    }
+
+}
