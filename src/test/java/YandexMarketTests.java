a527ed5dab59ab00ffbc18d304e7fb912609d99c
ais235 <ais235@yandex.ru>
2020-12-18 01:20:44 +0300

    yandex market tests

--- src/test/java/YandexMarketTests.java
+++ src/test/java/YandexMarketTests.java
@@ -2,25 +2,38 @@ import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 public class YandexMarketTests {
 
     @Test
-    void selenideSearchTest(){
-        //открыть яндекс 321
-        open("https://yandex.ru");
-        //Кликнуть на «Маркет»
-        $("a[data-id='market']").click();
-        //переход на соседнюю вкладку
-        switchTo().window(1);
-        //В поле поиска ввести «ноутбуки»
-        $("#header-search").setValue("ноутбуки");
-        //Проверить что поле поиска содержит «ноутбуки»
-        $("#header-search").shouldHave(Condition.value("ноутбуки"));
-        //Нажать «Найти»
-        $("button[type='submit']").click();
-        //Проверяем что нашлись ноутбуки
-        $("div[data-apiary-widget-name='@MarketNode/SearchHeadline']").shouldHave(Condition.text("Ноутбуки"));
+    public void selenideSearchTest(){
+        step("Открываем главную страницу яндекса", () -> {
+            open("https://yandex.ru");
+        });
+
+        step("Переходим в яндекс «Маркет»", () -> {
+            $("a[data-id='market']").click();
+        });
+
+        step("переход на соседнюю вкладку", () -> {
+            switchTo().window(1);
+        });
+
+        step("В поле поиска ввести «ноутбуки»", () -> {
+            $("#header-search").setValue("ноутбуки");
+        });
+
+        step("Проверить что поле поиска содержит «ноутбуки»", () -> {
+            $("#header-search").shouldHave(Condition.value("ноутбуки"));
+        });
+
+        step("Нажать «Найти»", () -> {
+            $("button[type='submit']").click();
+        });
+        step("Проверяем что нашлись ноутбуки", () -> {
+            $("div[data-apiary-widget-name='@MarketNode/SearchHeadline']").shouldHave(Condition.text("Ноутбуки"));
+        });
     }
 
 }
